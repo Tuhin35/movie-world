73b64c375e64da8338802d84503082761da33917
Tuhin35 <ariaan59@gmail.com>
2023-11-05 00:56:46 +0600

    commit

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -3,15 +3,17 @@ import { useSelector } from "react-redux";
 
 import "./style.scss";
 
-import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
 import Img from "../../../components/lazyLoadImage/Img";
 import avatar from "../../../assets/avatar.png";
+import ContentWrapper from "../../../components/ContentWrapper/ContentWrapper";
 
 const Cast = ({ data, loading }) => {
     const { url } = useSelector((state) => state.home);
                     // console.log(data)
                     // console.log(url)
     const skeleton = () => {
+        
         return (
             <div className="skItem">
                 <div className="circle skeleton"></div>
